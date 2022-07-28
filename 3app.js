b0b557d9ebcd357a186b45b5360535f3d4b9e3b0
thong1403 <110025148+thong1403@users.noreply.github.com>
2022-07-28 21:19:48 +0700

    bài tập về nhà

---
+++ 3app.js
@@ -0,0 +1,29 @@
+//1: alert("điểm của học sinh , sinh viên");
+// let a = Number(prompt("điểm vật lý"));
+// let b = Number(prompt("điểm hóa học")); 
+// let c = Number(prompt("điểm sinh học"));
+
+// let d = a + b + c;
+// let e = ((a + b + c) / 3);
+
+
+// alert(d);
+// alert(e);
+
+
+//2: let a = prompt("cho người dùng nhập độ c");
+// let b = (1.8 * a +32);
+// alert("chuyển đổ từ độ c sang độ f " + b);
+
+//4: let a = prompt("nhập bán kính hình tròn");
+// let b = 2;
+// let s = (Math.PI * a * b);
+// alert("chu vi hình tròn là " + s);
+
+//3: let a = prompt("nhập bán kính đường tròn");
+// let s = (Math.PI * a * a);
+// alert("diện tích hình tròn là " + s);
+
+
+
+
